bbcc2236a5cb37b534d22921535b802c7388244e
alexmudra <alexander.kobko@gmail.com>
2017-10-29 20:47:01 +0200

    3firstFiles

---
+++ 1.js
@@ -0,0 +1,6 @@
+function deductNumber (a, b){
+return a - b;
+}
+
+
+
---
+++ auth.js
@@ -0,0 +1,10 @@
+function authentificate (login , password){
+  if (login === 'login' && password === 'passford' {
+    return 'You were logged in';
+  } else {
+    return 'Login is incorrect';	  
+  }
+
+
+}
+
